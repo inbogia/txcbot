4c62c018a9faff0163cb1daf627cf0ba44c8129a
ingli0 <76855285+ingli0@users.noreply.github.com>
2021-01-05 16:34:04 +0200

    good

--- po.py
+++ po.py
@@ -55,11 +55,12 @@ async def _8ball(ctx, *, question):
 
 @client.command()
 @commands.has_permissions(manage_messages=True)
-async def clr(ctx, amount):
- if amount <20 :
-     await ctx.channel.purge(limit=amount+1)
- else:
-    await ctx.send(f'pola ebales')
+async def clr(ctx,amount=5):
+    if amount >26:
+        await ctx.send(f'pola ebales')
+    else:
+         await ctx.channel.purge(limit=amount+1)
+
 
 
 @client.command()
@@ -68,27 +69,6 @@ async def sm(ctx, seconds: int):
     await ctx.channel.edit(slowmode_delay=seconds)
     await ctx.send(f"ebala slow mode {seconds} deuterolepta!")
 
-'''
-class const:
-    def __init__(self):
-        
-        self.MODERATOR_ID = 607694678363471926
-        self.SYNADELFOS_ROLE_ID = 774337999307014174
-        self.MUTED_ROLE_ID = 786702739345375232
-
-
-    muted_role = ctx.guild.get_role(const.MUTED_ROLE_ID)
-
-    if muted_role in member.roles:
-        await ctx.send(f"{member.mention} is already muted")
-        return
-    
-    await member.add_roles(muted_role)
-    await ctx.send(f"{ctx.author.mention} muted {member.mention} for {minutes} minutes")
-
-    synadelfos_role = ctx.guild.get_role(const.SYNADELFOS_ROLE_ID)
-    await member.remove_roles(synadelfos_role)
-    '''
 
 
 @client.command()
@@ -113,6 +93,7 @@ async def _bet(ctx, omada1, *,omada2):
     'over',
     'under']
     await ctx.send(f'match: {omada1}-{omada2}\nna to paiksis: {random.choice(apan)}') 
+
 
 
 
